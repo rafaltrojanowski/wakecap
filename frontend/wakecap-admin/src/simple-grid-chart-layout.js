63741c2a05f485df03f02069dbfcc29f20d63f09
Rafał Trojanowski <rt.trojanowski@gmail.com>
2018-11-13 12:45:19 +0700

    add custom grids

---
+++ frontend/wakecap-admin/src/simple-grid-chart-layout.js
@@ -0,0 +1,97 @@
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import CardContentInner from './card-content-inner';
+import Labeled from './labeled';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  wrapper: {
+    display: 'grid',
+    gridTemplateColumns: '100%', // TODO make it work
+  },
+};
+
+const sanitizeRestProps = ({
+    children,
+    className,
+    record,
+    resource,
+    basePath,
+    version,
+    initialValues,
+    translate,
+    ...rest
+}) => rest;
+
+/**
+ * Inspired by: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout/Basic_Concepts_of_Grid_Layout
+ * Based on: https://github.com/marmelab/react-admin/blob/master/packages/ra-ui-materialui/src/detail/SimpleShowLayout.js
+ */
+
+export const SimpleGridChartLayout = ({
+    basePath,
+    className,
+    children,
+    record,
+    resource,
+    version,
+    ...rest
+}) => (
+    <CardContentInner
+        className={className}
+        key={version}
+        {...sanitizeRestProps(rest)}
+    >
+        <div style={styles.wrapper}>
+          {Children.map(
+              children,
+              field =>
+                  field ? (
+                      <div
+                          key={field.props.source}
+                          className={classnames(
+                              `ra-field ra-field-${field.props.source}`,
+                              field.props.className
+                          )}
+                      >
+                          {field.props.addLabel ? (
+                              <Labeled
+                                  record={record}
+                                  resource={resource}
+                                  basePath={basePath}
+                                  label={field.props.label}
+                                  source={field.props.source}
+                                  disabled={false}
+                              >
+                                  {field}
+                              </Labeled>
+                          ) : typeof field.type === 'string' ? (
+                              field
+                          ) : (
+                              React.cloneElement(field, {
+                                  record,
+                                  resource,
+                                  basePath,
+                              })
+                          )}
+                      </div>
+                  ) : null
+          )}
+        </div>
+    </CardContentInner>
+);
+
+SimpleGridChartLayout.propTypes = {
+    styles: PropTypes.object,
+    basePath: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.node,
+    record: PropTypes.object,
+    resource: PropTypes.string,
+    version: PropTypes.number,
+};
+
+export default withStyles(styles)(SimpleGridChartLayout);
--- frontend/wakecap-admin/src/users/user-show.js
+++ frontend/wakecap-admin/src/users/user-show.js
@@ -21,6 +21,9 @@ import {
 import { UserList } from './user-list';
 import dataProvider from './../dataProvider';
 
+import SimpleGridLayout from './../simple-grid-layout';
+import SimpleGridChartLayout from './../simple-grid-chart-layout';
+
 const Aside = ({ record }) => (
     <div style={{ width: 80, margin: '1em' }}>
         <Typography variant="title">Supervisor</Typography>
@@ -37,9 +40,7 @@ export class UserShow extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      weeklyHours: null
-    }
+    this.state = { weeklyHours: null }
   }
 
   componentDidMount() {
@@ -78,14 +79,21 @@ export class UserShow extends Component {
             <UserList {...props}/>
           </Grid>
           <Grid item sm={8} xs={12}>
-            <Show aside={<Aside />} title={title} {...props}>
-              <SimpleShowLayout>
+            <Show title={title} {...props}>
+              <SimpleGridLayout>
                 <TextField source="name" />
+                <div></div>
+                <div></div>
+                <TextField source="supervisor" />
                 <TextField source="total_hours" />
                 <TextField source="last_active_zone" />
+                <div></div>
+                <div></div>
+              </SimpleGridLayout>
+            </Show>
 
-                <Divider />
-
+            <Show title={title} {...props} actions={null}>
+              <SimpleGridChartLayout>
                 <ResponsiveContainer width="100%" height={200}>
                   <BarChart data={weeklyHours}>
                     <XAxis dataKey="date"/>
@@ -99,8 +107,11 @@ export class UserShow extends Component {
                     </Bar>
                   </BarChart>
                 </ResponsiveContainer>
+              </SimpleGridChartLayout>
+            </Show>
 
-                <Divider />
+            <Show title={title} {...props} actions={null}>
+              <SimpleShowLayout>
 
                 <ReferenceManyField label="Assigned Helmets" reference="helmets" target="user_id">
                   <Datagrid>
